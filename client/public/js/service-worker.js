8c319538556924d0c4aff78144928f6db45e539b
ZachWagner1 <zwag14@yahoo.com>
2020-10-27 18:06:05 -0500

    service worker started

---
+++ client/public/js/service-worker.js
@@ -0,0 +1,57 @@
+const FILES_TO_CACHE = [
+    "../../src/pages/AllProjects.js",
+    "../../src/pages/Dashboard.js",
+    "../../src/pages/Detail.js",
+    "../../src/pages/Home.js",
+    "../../src/pages/Login.js",
+    "../../src/pages/Project.js",
+    "../../src/pages/Signup.js",
+]
+
+const APP_PREFIX = 'Words-projects';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            // store current cache
+            cacheKeeplist.push(CACHE_NAME);
+
+            // delete old cache
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache: ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }))
+        })
+    )
+})
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+}) 
